b489c861ef6a3e3a8073f05261dcfbf1206ebd53
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2021-12-27 17:44:30 -0300

    Separação das datas por mês

--- dadosCovid.py
+++ dadosCovid.py
@@ -4,13 +4,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from fpdf import FPDF
 
-
-linkCSV = 'https://www.seade.gov.br/wp-content/uploads/coronavirus-files/Dados-covid-19-estado.csv'
-nomeCSV = 'dadosCovidEstado.csv'
+linkArq = 'https://www.seade.gov.br/wp-content/uploads/coronavirus-files/Dados-covid-19-estado.csv'
+nomeArq = 'dadosCovidEstado.csv'
 
 try:
-  os.remove(nomeCSV)
+  os.remove(nomeArq)
 except OSError as e:
   print(f"Erro: {e.strerror}")
 
-wget.download(linkCSV, nomeCSV)
+wget.download(linkArq, nomeArq, False)
+
+tabela = pd.read_csv(nomeArq, delimiter=";", encoding='ISO-8859-1')
+
+data = tabela['Data']
+totalCasos = tabela['Total de casos']
+casosDia = tabela['Casos por dia']
+obitosDia = tabela['Óbitos por dia']
+
+meses = []
+casosMes = []
+
+for i in range(len(data)):
+  [dia, mes, ano] = data[i].split('/')
+  dado = f'{mes} {ano}'
+
+  if dado not in meses:
+    meses.append(dado)
+
+print(meses)
+print(casosMes)
+
+print(len(meses))
+print(len(casosMes))
+# plt.bar(meses, casosMes)
+# plt.show()
