c2becfaead385194b78c6fa16f260f53a15336b9
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2021-12-30 12:02:38 -0300

    Formatação dos gráficos

--- dadosCovid.py
+++ dadosCovid.py
@@ -6,6 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from fpdf import FPDF
 
+def formataNum(num, sep='.'):
+  return num if len(num) <= 3 else formataNum(num[:-3], sep) + sep + num[-3:]
+
+def adicionaLegendaNasBarras(grafico):
+  for reta in grafico:
+    altura = reta.get_height()
+    largura = reta.get_width()
+    plt.text(reta.get_x() + largura/2, altura, formataNum(str(int(altura))), ha='center', va='bottom')
+
 linkArq = 'https://www.seade.gov.br/wp-content/uploads/coronavirus-files/Dados-covid-19-estado.csv'
 nomeArq = 'dadosCovidEstado.csv'
 
@@ -57,26 +66,24 @@ for i in range(len(meses)):
   obitosMes.append(quantObitos)
   quantObitos = 0
 
-# print(meses)
-# print(casosMes)
-# print(len(meses))
-# print(len(casosMes))
-# print(obitosMes)
-
-plt.figure(figsize=(14, 6))
-plt.bar(meses, casosMes, ec = "k", alpha = .6, color = "royalblue")
+plt.figure(figsize=(16, 7))
+fig1 = plt.bar(meses, casosMes, ec="k", alpha=.6, color="royalblue")
 plt.xticks(np.arange(0, len(meses), 2))
 plt.title('Casos novos por mês')
 plt.xlabel('Mês')
 plt.ylabel('Quantidade de casos')
+plt.tight_layout()
+adicionaLegendaNasBarras(fig1)
 plt.savefig('CasosMes.png', format='png', dpi=300)
 plt.show()
 
-plt.figure(figsize=(14, 6))
-plt.bar(meses, obitosMes, ec = "k", alpha = .6, color = "royalblue")
+plt.figure(figsize=(16, 7))
+fig2 = plt.bar(meses, obitosMes, ec="k", alpha=.6, color="royalblue")
 plt.xticks(np.arange(0, len(meses), 2))
 plt.title('Óbitos por mês')
 plt.xlabel('Mês')
 plt.ylabel('Quantidade de óbitos')
+plt.tight_layout()
+adicionaLegendaNasBarras(fig2)
 plt.savefig('ObitosMes.png', format='png', dpi=300)
 plt.show()
